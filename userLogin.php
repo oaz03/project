d28777b20e7f5dec64f66fea472f2247d3cd6239
ericnn250 <38242660+ericnn250@users.noreply.github.com>
2018-04-10 11:25:43 +0200

    oiuytr

---
+++ userLogin.php
@@ -0,0 +1,32 @@
+<?php
+include 'dbconnect.php';
+if (isset($_POST['login'])) {
+	session_start();
+	$username=mysqli_real_escape_string($conn, $_POST['username']);
+	$password=mysqli_real_escape_string($conn, $_POST['pass']);
+         if(empty($username)||empty($password)){
+         header("Location: ../project/login.php?login=empty");
+ 	           exit();
+            }
+        else{
+
+       $sql="select * from user where username='$username'and password='$password'";
+       $result=mysqli_query($conn,$sql);
+       $resultchek=mysqli_num_rows($result);
+       if ($resultchek<1) {
+       header("Location: ../project/login.php?login=invalid");
+ 	           exit();
+       }
+       else{
+       $_SESSION['surenames']=$row['surename'];
+       	header("Location: ../project/dashboard.php?login=admin");
+         }
+       }
+      }
+else
+{
+	header("Location: ../project/login.php?login=invalid");
+ 	           exit();
+}
+
+?>
