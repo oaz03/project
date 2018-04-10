3fcdeb435cef0b87e6eae4f8509a8b808d61c88a
anaclet922 <37176750+anaclet922@users.noreply.github.com>
2018-04-10 11:23:36 +0200

    ffff

--- dashboard.php
+++ dashboard.php
@@ -5,8 +5,22 @@
 			<li style="<?php if($_GET['menu']=="editor") echo 'background-color: #ffcccc'?>"><a href="Dashboard.php?menu=editor"><i class="ion-edit" style="font-size: 20px;font-weight: bold;"></i>Editor</a></li>
 			<li style="<?php if($_GET['menu']=="posts") echo 'background-color: #ffcccc'?>"><a href="Dashboard.php?menu=posts"><i class="ion-ios-paper" style="font-size: 20px;font-weight: bold;"></i>Posts</a></li>
 			<li style="<?php if($_GET['menu']=="manage_users") echo 'background-color: #ffcccc'?>"><a href="Dashboard.php?menu=manage_users"><i class="ion-android-people" style="font-size: 20px;font-weight: bold;"></i> Manage Users</a></li>
-		</ul></div>
-		<div class="col-lg-4">helllo</div>
+		</ul>
+<?php
+@$menu=$_GET['menu'];
+@$id=$_GET['id'];
+if($menu=="editor")
+   include('Editor.php');
+else if($menu=="posts")
+	include('posts.php');
+else if($menu=="manage_users")
+    include('manage_users.php');
+else if($id)
+	include('Editor.php');
+
+?>
+	</div>
+		<div class="col-lg-4">helllohelllohelllohellloecho</div>
 </div>
 
 <style type="text/css">
---
+++ editor.php
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+<form method="POST" class="form">
+	<input type="text" name="title" placeholder="Title" class="form-control"><br>
+	<textarea class="ckeditor" name="ckeditor" class="form-control"></textarea>
+</form>
