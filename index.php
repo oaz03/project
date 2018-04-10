b98485ee1da3b432fcf8ccddc6ceb9e8d773ca85
ericnn250 <38242660+ericnn250@users.noreply.github.com>
2018-04-10 10:39:22 +0200

    wertyj

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
-hello
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
-
+	<form method="POST" action="">
+		<input type="text" name="username">
+		<input type="password" name="pass">
+		<button type="submit">Login</button>
+	</form>
 </body>
 </html>
