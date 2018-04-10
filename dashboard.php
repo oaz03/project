65e1b50cf40673b4455710593f112de25b3d4a78
anaclet922 <37176750+anaclet922@users.noreply.github.com>
2018-04-10 11:53:13 +0200

    tfyghjikl

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,12 @@
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<?php
+include('_header.php');
+?>
 <div class="row">
 	<div class="col-lg-8" id="dashMenu">
 	<ul>
-			<li style="<?php if($_GET['menu']=="editor") echo 'background-color: #ffcccc'?>"><a href="Dashboard.php?menu=editor"><i class="ion-edit" style="font-size: 20px;font-weight: bold;"></i>Editor</a></li>
-			<li style="<?php if($_GET['menu']=="posts") echo 'background-color: #ffcccc'?>"><a href="Dashboard.php?menu=posts"><i class="ion-ios-paper" style="font-size: 20px;font-weight: bold;"></i>Posts</a></li>
-			<li style="<?php if($_GET['menu']=="manage_users") echo 'background-color: #ffcccc'?>"><a href="Dashboard.php?menu=manage_users"><i class="ion-android-people" style="font-size: 20px;font-weight: bold;"></i> Manage Users</a></li>
+			<li style="<?php if($_GET['menu']=="editor") echo 'background-color: #00e600'?>"><a href="Dashboard.php?menu=editor"><i class="ion-edit" style="font-size: 20px;font-weight: bold;"></i>Editor</a></li>
+			<li style="<?php if($_GET['menu']=="posts") echo 'background-color: #00e600'?>"><a href="Dashboard.php?menu=posts"><i class="ion-ios-paper" style="font-size: 20px;font-weight: bold;"></i>Posts</a></li>
+			<li style="<?php if($_GET['menu']=="manage_users") echo 'background-color: #00e600'?>"><a href="Dashboard.php?menu=manage_users"><i class="ion-android-people" style="font-size: 20px;font-weight: bold;"></i> Manage Users</a></li>
 		</ul>
 <?php
 @$menu=$_GET['menu'];
--- editor.php
+++ editor.php
@@ -1,5 +1,6 @@
 <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
 <form method="POST" class="form">
 	<input type="text" name="title" placeholder="Title" class="form-control"><br>
-	<textarea class="ckeditor" name="ckeditor" class="form-control"></textarea>
+	<textarea class="ckeditor" name="ckeditor" class="form-control"></textarea><br>
+	<center><button type="submit" class="btn btn-success">Publish</button></center>
 </form>
